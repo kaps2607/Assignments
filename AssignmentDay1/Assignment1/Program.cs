db87868603fd90b934323aa09fc3d00b73afb820
kaps2607 <kapillund29@gmail.com>
2025-02-24 18:17:04 +0530

    Initial commit

---
+++ AssignmentDay1/Assignment1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Assignment 1 & 2
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            //int age;
+            bool isNumber = int.TryParse(Console.ReadLine(), out int age);
+            if (!isNumber)
+            {
+                Console.WriteLine("Invalid input! Please enter a number");
+            }
+            Console.Write("Enter your Percentage: ");
+            isNumber = double.TryParse(Console.ReadLine(), out double per);
+            if (!isNumber)
+            {
+                Console.WriteLine("Invalid input! Please enter valid percentage also without '%'");
+            }
+
+            Console.WriteLine($"Your name: {name}\nYour age: {age}\nYour Percentage: {per}");
+            #endregion
+            #region Assignment 3
+            Console.Write("Enter your email: ");
+            string email=Console.ReadLine();
+            //Console.WriteLine(email);
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                Console.WriteLine("You didn't enter anything! Please enter your emailId.");
+            }
+            else
+            {
+                Console.WriteLine($"Welcome, {email}");
+            }
+            #endregion
+
+        }
+    }
+}
---
+++ AssignmentDay1/Assignment4/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string pname=Console.ReadLine();
+            Console.Write("Enter admitting date(dd/MM/yyyy): ");
+            string date=Console.ReadLine();
+
+            bool isValid = DateTime.TryParse(date, out DateTime admitDate);
+            if (!isValid)
+            {
+                Console.WriteLine("Invalid date format! Please enter a valid date.");
+            }
+
+            Console.Write($"Enter discharged date if Mr./Ms. Patient is discharged(dd/MM/yyyy): ");
+            isValid = DateTime.TryParse(Console.ReadLine(), out DateTime disDate);
+            if (!isValid)
+            {
+                Console.WriteLine("Invalid date format! Please enter a valid date.");
+            }
+            else
+            {
+
+            }
+            Console.WriteLine(disDate);
+
+            
+
+        }
+    }
+}
