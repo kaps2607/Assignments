a44b36e779c03908008f808295762533206ff0d2
kaps2607 <kapillund29@gmail.com>
2025-03-11 17:44:14 +0530

    finalHackathonInsuranceAppDB

--- HackthonTask_InsuranceDB/obj/Debug/net6.0/HackthonTask.AssemblyInfo.cs
+++ HackthonTask_InsuranceDB/obj/Debug/net6.0/HackthonTask.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HackthonTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919d1f08858e396097aee8ac31f848fd4fdf6aea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1bba6b23145f58f42624ad745262334d3ddc5bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("HackthonTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HackthonTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
