5aee060a24b6975f3e1c7d44044e6f05a3038f9d
kaps2607 <kapillund29@gmail.com>
2025-03-26 19:00:41 +0530

    ECommerceInitialCommit

---
+++ ECommerce_Application/ECommerce.Infrastructure/Context/ECommerceApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Context
+{
+    public class ECommerceApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
